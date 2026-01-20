37da953f7f684c667e1b60b507f32eb1f296f81e
Rodrigo Vilina <rodrigovilina@agendapro.com>
2026-01-20 19:16:56 -0300

    feat: add `ExceptionHandlers::Failure`

---
+++ lib/platform/exception_handlers/failure.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Platform
+  module ExceptionHandlers
+    class Failure
+      def initialize(exception)
+        @exception = exception
+        freeze
+      end
+
+      def body
+        {
+          error: @exception.result[:error],
+          detail: @exception.result[:detail]
+        }
+      end
+
+      def status
+        @exception.result[:status] || :unprocessable_content
+      end
+
+      def log
+        NewRelic::Agent.notice_error(@exception)
+        Rails.logger.error "ExceptionHandler(#{status}): #{@exception.inspect}"
+        Rails.logger.error @exception.backtrace.join("\n")
+      end
+    end
+  end
+end
