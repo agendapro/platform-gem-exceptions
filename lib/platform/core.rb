049fbf0f4dd0b246d59ca6130c788cd0ff645aed
Rodrigo Vilina <rodrigovilina@agendapro.com>
2026-01-20 14:45:21 -0300

    feat: update entry file

--- lib/platform/core.rb
+++ lib/platform/core.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 require_relative "core/version"
+require 'service_actor'
+require_relative "../service_actor/checks/nil_check"
+require_relative "error_message"
+require_relative "service"
 
 module Platform
   module Core
     class Error < StandardError; end
-    # Your code goes here...
   end
 end
--- spec/platform/core_spec.rb
+++ spec/platform/core_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Platform::Core do
   it "has a version number" do
     expect(Platform::Core::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
